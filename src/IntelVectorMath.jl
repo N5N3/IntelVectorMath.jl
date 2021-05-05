18f064a8e34ae2e127b8cb6608904c2e71210871
N5N3 <2642243996@qq.com>
2021-05-06 00:52:57 +0800

    Update IntelVectorMath.jl
    Use FunI to support Inc

--- src/IntelVectorMath.jl
+++ src/IntelVectorMath.jl
@@ -17,55 +17,55 @@ end
 
 for t in (Float32, Float64, ComplexF32, ComplexF64)
     # Unary, real or complex
-    def_unary_op(t, t, :acos, :acos!, :Acos)
-    def_unary_op(t, t, :asin, :asin!, :Asin)
-    def_unary_op(t, t, :acosh, :acosh!, :Acosh)
-    def_unary_op(t, t, :asinh, :asinh!, :Asinh)
-    def_unary_op(t, t, :sqrt, :sqrt!, :Sqrt)
-    def_unary_op(t, t, :exp, :exp!, :Exp)
-    def_unary_op(t, t, :log, :log!, :Ln)
+    def_unary_op(t, t, :acos, :acos!, :AcosI)
+    def_unary_op(t, t, :asin, :asin!, :AsinI)
+    def_unary_op(t, t, :acosh, :acosh!, :AcoshI)
+    def_unary_op(t, t, :asinh, :asinh!, :AsinhI)
+    def_unary_op(t, t, :sqrt, :sqrt!, :SqrtI)
+    def_unary_op(t, t, :exp, :exp!, :ExpI)
+    def_unary_op(t, t, :log, :log!, :LnI)
 
     # # Binary, real or complex
-    def_binary_op(t, t, :pow, :pow!, :Pow, true)
-    def_binary_op(t, t, :divide, :divide!, :Div, true)
+    def_binary_op(t, t, :pow, :pow!, :PowI, true)
+    def_binary_op(t, t, :divide, :divide!, :DivI, true)
 end
 
 for t in (Float32, Float64)
     # Unary, real-only
-    def_unary_op(t, t, :cbrt, :cbrt!, :Cbrt)
-    def_unary_op(t, t, :expm1, :expm1!, :Expm1)
-    def_unary_op(t, t, :log1p, :log1p!, :Log1p)
-    def_unary_op(t, t, :abs, :abs!, :Abs)
-    def_unary_op(t, t, :abs2, :abs2!, :Sqr)
-    def_unary_op(t, t, :ceil, :ceil!, :Ceil)
-    def_unary_op(t, t, :floor, :floor!, :Floor)
-    def_unary_op(t, t, :round, :round!, :Round)
-    def_unary_op(t, t, :trunc, :trunc!, :Trunc)
+    def_unary_op(t, t, :cbrt, :cbrt!, :CbrtI)
+    def_unary_op(t, t, :expm1, :expm1!, :Expm1I)
+    def_unary_op(t, t, :log1p, :log1p!, :Log1pI)
+    def_unary_op(t, t, :abs, :abs!, :AbsI)
+    def_unary_op(t, t, :abs2, :abs2!, :SqrI)
+    def_unary_op(t, t, :ceil, :ceil!, :CeilI)
+    def_unary_op(t, t, :floor, :floor!, :FloorI)
+    def_unary_op(t, t, :round, :round!, :RoundI)
+    def_unary_op(t, t, :trunc, :trunc!, :TruncI)
 
     # now in SpecialFunctions (make smart, maybe?)
-    def_unary_op(t, t, :erf, :erf!, :Erf)
-    def_unary_op(t, t, :erfc, :erfc!, :Erfc)
-    def_unary_op(t, t, :erfinv, :erfinv!, :ErfInv)
-    def_unary_op(t, t, :erfcinv, :erfcinv!, :ErfcInv)
-    def_unary_op(t, t, :lgamma, :lgamma!, :LGamma)
-    def_unary_op(t, t, :gamma, :gamma!, :TGamma)
+    def_unary_op(t, t, :erf, :erf!, :ErfI)
+    def_unary_op(t, t, :erfc, :erfc!, :ErfcI)
+    def_unary_op(t, t, :erfinv, :erfinv!, :ErfInvI)
+    def_unary_op(t, t, :erfcinv, :erfcinv!, :ErfcInvI)
+    def_unary_op(t, t, :lgamma, :lgamma!, :LGammaI)
+    def_unary_op(t, t, :gamma, :gamma!, :TGammaI)
     # Not in Base
-    def_unary_op(t, t, :inv_cbrt, :inv_cbrt!, :InvCbrt)
-    def_unary_op(t, t, :inv_sqrt, :inv_sqrt!, :InvSqrt)
-    def_unary_op(t, t, :pow2o3, :pow2o3!, :Pow2o3)
-    def_unary_op(t, t, :pow3o2, :pow3o2!, :Pow3o2)
+    def_unary_op(t, t, :inv_cbrt, :inv_cbrt!, :InvCbrtI)
+    def_unary_op(t, t, :inv_sqrt, :inv_sqrt!, :InvSqrtI)
+    def_unary_op(t, t, :pow2o3, :pow2o3!, :Pow2o3I)
+    def_unary_op(t, t, :pow3o2, :pow3o2!, :Pow3o2I)
 
     # Enabled only for Real. MKL guarantees higher accuracy, but at a
     # substantial performance cost.
-    def_unary_op(t, t, :atan, :atan!, :Atan)
-    def_unary_op(t, t, :cos, :cos!, :Cos)
-    def_unary_op(t, t, :sin, :sin!, :Sin)
-    def_unary_op(t, t, :tan, :tan!, :Tan)
-    def_unary_op(t, t, :atanh, :atanh!, :Atanh)
-    def_unary_op(t, t, :cosh, :cosh!, :Cosh)
-    def_unary_op(t, t, :sinh, :sinh!, :Sinh)
-    def_unary_op(t, t, :tanh, :tanh!, :Tanh)
-    def_unary_op(t, t, :log10, :log10!, :Log10)
+    def_unary_op(t, t, :atan, :atan!, :AtanI)
+    def_unary_op(t, t, :cos, :cos!, :CosI)
+    def_unary_op(t, t, :sin, :sin!, :SinI)
+    def_unary_op(t, t, :tan, :tan!, :TanI)
+    def_unary_op(t, t, :atanh, :atanh!, :AtanhI)
+    def_unary_op(t, t, :cosh, :cosh!, :CoshI)
+    def_unary_op(t, t, :sinh, :sinh!, :SinhI)
+    def_unary_op(t, t, :tanh, :tanh!, :TanhI)
+    def_unary_op(t, t, :log10, :log10!, :Log10I)
 
     # # .^ to scalar power
     # mklfn = Base.Meta.quot(Symbol("$(vml_prefix(t))Powx"))
@@ -86,16 +86,16 @@ for t in (Float32, Float64)
     # end
 
     # # Binary, real-only
-    def_binary_op(t, t, :atan, :atan!, :Atan2, false)
-    def_binary_op(t, t, :hypot, :hypot!, :Hypot, false)
+    def_binary_op(t, t, :atan, :atan!, :Atan2I, false)
+    def_binary_op(t, t, :hypot, :hypot!, :HypotI, false)
 
     # Unary, complex-only
-    def_unary_op(Complex{t}, Complex{t}, :conj, :conj!, :Conj)
-    def_unary_op(Complex{t}, t, :abs, :abs!, :Abs)
-    def_unary_op(Complex{t}, t, :angle, :angle!, :Arg)
+    def_unary_op(Complex{t}, Complex{t}, :conj, :conj!, :ConjI)
+    def_unary_op(Complex{t}, t, :abs, :abs!, :AbsI)
+    def_unary_op(Complex{t}, t, :angle, :angle!, :ArgI)
 
     ### cis is special, IntelVectorMath function is based on output
-    def_unary_op(t, Complex{t}, :cis, :cis!, :CIS; vmltype = Complex{t})
+    def_unary_op(t, Complex{t}, :cis, :cis!, :CISI; vmltype = Complex{t})
 
     # Binary, complex-only. These are more accurate but performance is
     # either equivalent to Base or slower.
